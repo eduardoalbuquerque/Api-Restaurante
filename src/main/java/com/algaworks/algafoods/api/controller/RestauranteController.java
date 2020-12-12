cd24d58e484355a676368e8786d3423c067f427f
Eduardo Albuquerque <eduardo.albuquerque@hotmail.com.br>
2020-12-12 20:10:16 -0300

    Add Path Restaurante

--- src/main/java/com/algaworks/algafoods/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafoods/api/controller/RestauranteController.java
@@ -1,17 +1,15 @@
 package com.algaworks.algafoods.api.controller;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.ReflectionUtils;
+import org.springframework.web.bind.annotation.*;
 
 import com.algaworks.algafoods.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafoods.domain.model.Restaurante;
@@ -80,6 +78,35 @@ public class RestauranteController {
 			return ResponseEntity.badRequest()
 					.body(e.getMessage());
 		}
+	}
+
+	@PatchMapping("/{restauranteId}")
+	public ResponseEntity<?> atualizarParial(@PathVariable Long restauranteId,
+											 @RequestBody Map<String,Object> campos){
+		Restaurante restaurante = cadastroRestaurante.burcar(restauranteId);
+		if(restaurante == null){
+			ResponseEntity.notFound().build();
+		}
+		merge(campos,restaurante);
+		return atualizar(restauranteId, restaurante);
+
+	}
+
+	private void  merge(Map<String, Object> camposOrigem, Restaurante restauranteDestino){
+
+		ObjectMapper objectMapper = new ObjectMapper();
+		Restaurante restauranteOrigem = objectMapper.convertValue(camposOrigem,Restaurante.class);
+
+		camposOrigem.forEach((key, value) -> {
+			//retorna as propriedades da class :: findField
+			Field field = ReflectionUtils.findField(Restaurante.class,key);
+			field.setAccessible(true); //permitir acesso a propriedades cmo visibilidade private
+
+			//agora pegando o valor da propriedade e nao mais o nome da propriedade :: getField
+			Object novoValor = ReflectionUtils.getField(field, restauranteOrigem);
+
+			ReflectionUtils.setField(field,restauranteDestino,novoValor);
+		});
 	}
 
 }
